bffa97877697678ec3a6701a307f64379dba8340
Prajjawal Agrawal <114766095+prajjawal97@users.noreply.github.com>
2023-10-11 13:09:53 +0530

    Update SampleSecurityTest.java

--- src/test/java/com/seleniumtests/tests/security/SampleSecurityTest.java
+++ src/test/java/com/seleniumtests/tests/security/SampleSecurityTest.java
@@ -7,27 +7,27 @@ import org.zaproxy.clientapi.core.ClientApiException;
 
 public class SampleSecurityTest extends BaseSecurity {
 
-    private static final String SITE = "https://juice-shop.herokuapp.com/";
-   // private static final String SITE = "https://www.flipkart.com/";
+    //private static final String SITE = "https://juice-shop.herokuapp.com/";
+    private static final String Website = "https://www.flipkart.com/";
     @Test()
     public void spiderHomePage() throws ClientApiException, InterruptedException {
-        getDriver().get(SITE);
-        spiderTarget(SITE);
+        getDriver().get(Website);
+        spiderTarget(Website);
     }
 
     @Test()
     public void passiveScanHomePage() throws ClientApiException {
-        getDriver().get(SITE);
+        getDriver().get(Website);
         // some more logic using page object to move to different pages goes here
         waitForPassiveScanToComplete();
-        checkRiskCount(SITE);
+        checkRiskCount(Website);
     }
 
     @Test()
     public void activeScanHomePage() throws ClientApiException, InterruptedException {
-        getDriver().get(SITE);
-        activeScan(SITE);
-        checkRiskCount(SITE);
+        getDriver().get(Website);
+        activeScan(Website);
+        checkRiskCount(Website);
     }
 
 }
